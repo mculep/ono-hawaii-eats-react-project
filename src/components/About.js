41d72d7cdeec92de0f000f87078cff4a35382f96
mculep <mculep@gmail.com>
2021-04-16 00:42:45 -0700

    revise about

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,8 @@ import React from 'react'
 const About = () => {
     return (
         <div className="about-section">
-             <h1>Aloha! Welcome to 'Ono Hawaii Eats!</h1>
-            <p>'Ono in Hawaiian means "delicious." I created this App because I was born and raised in Hawaii! Since I have years of experience on the island, hundreds of people ask me "where is the best place to eat when I come visit?" Usually, I send tons of links via email or text- but now they can see all of the highly rated eateries conveniently in one place!
+             <h1>Aloha! I'm Melody Ulep. Welcome to 'Ono Hawaii Eats!</h1>
+            <p>'Ono in Hawaiian means "delicious." I created this App because I was born and raised in Hawaii! Since I have years of experience on the island, hundreds of people ask me "where is the best place to eat when I come visit?" Usually, I send tons of links via email or text- but now they can see all of the highly rated eateries conveniently in one place! 
             </p>
         </div>
     )
